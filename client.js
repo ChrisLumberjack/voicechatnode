a724eee2939fbe05746fcc289ab86a3592dab4e5
Cristian Pérez <97258847+ChrisLumberjack@users.noreply.github.com>
2024-08-03 16:38:23 +0200

    Update client.js

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 let name;
 let connectedUser;
 
-const conn = new WebSocket("wss://0.0.0.0:9090");
+const conn = new WebSocket("wss://localhost:9090");
 
 conn.onopen = function () {
   console.log("Connected to the signaling server");
